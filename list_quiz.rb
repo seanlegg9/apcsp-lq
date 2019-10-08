96f53fc7322badfcea1c8c283d49f3bf6d52c632
seanlegg9 <slegg@gmail.com>
2019-10-08 09:59:34 -0600

    Without series_up

--- list_quiz.rb
+++ list_quiz.rb
@@ -18,3 +18,18 @@ end
 # puts three_even?([2, 1, 4, 6]) # false
 # puts three_even?([21, 4, 6, 4]) # true
 # puts three_even?([]) # false
+
+def bigger_two(list_1, list_2)
+    sum_1 = list_1[0].to_i + list_1[1].to_i
+    sum_2 = list_2[0].to_i + list_2[1].to_i
+    if sum_1 > sum_2
+        return list_1
+    elsif sum_2 > sum_1
+        return list_2
+    else
+        return list_1
+    end
+end
+
+# print bigger_two([1, 2], [3, 4]) # [3, 4]
+# print bigger_two([1, 7], [4, 4]) # [1, 7]
